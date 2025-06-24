054834a77c2867d939c470895bac527b0a34535b
Ayushpaney24 <ayushpandey4480@gmail.com>
2025-06-24 21:00:06 +0530

    responsive

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+ const toggleBtn = document.getElementById("menu-toggle");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  toggleBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+
+
 const root = document.documentElement;
 const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
 const marqueeContent = document.querySelector("ul.marquee-content-item  ");
